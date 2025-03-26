034b954f481f5f82eff50025dc91c37b336a05bb
unknown <manh.dd235774@sis.hust.edu.vn>
2025-03-26 23:38:01 +0700

    asm1.c

---
+++ W4/asm1b.c
@@ -0,0 +1,14 @@
+//1b_a
+#include <stdio.h>
+#define MAX_LENGTH 40
+int main() {
+    char str[MAX_LENGTH];
+    int n;
+
+    scanf("%d", &n);
+    scanf("%s", str);
+
+    printf("%s\n", str);
+    printf("%d\n", n);
+    return 0;
+}
